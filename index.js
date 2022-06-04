5da1d108c0c85c3879f593f69577909f43cd5369
+ <ronoh.ivy@gmail.com>
2022-06-04 04:56:40 +0300

    commit tree 6f37bdedc13edc20efec98949d2345dd29077036

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Follow along with the examples here
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+    else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    else if (string.toLowerCase()) {
+        return "I can't hear you!";
+    }
+}
